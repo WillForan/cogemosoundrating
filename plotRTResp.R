8417882b45bc069c02dbeb7d5e34ee982ce856b2
Will Foran <willforan@gmail.com>
2013-09-26 17:56:34 -0400

    add Rplots

---
+++ plotRTResp.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+library(reshape2)
+resps <- read.table('BigList.txt',header=T)
+names(resps) <- c('Subj','Date','Sound','RT.arousal' , 'RESP.arousal','RT.valence','RESP.valence')
+resps$Sound <- gsub('.wav','',resps$Sound)
+
+resps.reshaped<-reshape(resps,direction="long",varying=c('RT.arousal','RT.valence','RESP.arousal','RESP.valence'),timevar="Scale")
+
+#ggplot(resps.reshaped,aes(group=Sound,fill=Sound,x=RESP))+geom_density(aes(group=Sound),alpha=I(.3))+facet_grid(Scale~.)+theme_bw()
+jitterp <- ggplot(resps.reshaped,aes(y=Scale,x=RESP,size=RT,color=Scale))+geom_jitter(height=.05,width=0)+facet_wrap(~Sound)+theme_bw()
+ggsave(jitterp,file='jitterplot.png',width=8,height=8)
+p <- ggplot(resps.reshaped,aes(group=Sound,x=RESP))+geom_histogram()+facet_grid(Sound~Scale)+theme_bw()
+ggsave(p,file='histograms.png',width=8,height=40)
