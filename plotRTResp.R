ea390d7c43ef5f5caaac07fe43ff145d05c9855d
Will Foran <willforan@gmail.com>
2013-09-27 17:10:31 -0400

    add long version of list

--- plotRTResp.R
+++ plotRTResp.R
@@ -5,6 +5,7 @@ names(resps) <- c('Subj','Date','Sound','RT.arousal' , 'RESP.arousal','RT.valenc
 resps$Sound <- gsub('.wav','',resps$Sound)
 
 resps.reshaped<-reshape(resps,direction="long",varying=c('RT.arousal','RT.valence','RESP.arousal','RESP.valence'),timevar="Scale")
+write.csv(resps.reshaped, file="BigList_Long.csv")
 
 #ggplot(resps.reshaped,aes(group=Sound,fill=Sound,x=RESP))+geom_density(aes(group=Sound),alpha=I(.3))+facet_grid(Scale~.)+theme_bw()
 jitterp <- ggplot(resps.reshaped,aes(y=Scale,x=RESP,size=RT,color=Scale))+geom_jitter(height=.05,width=0)+facet_wrap(~Sound)+theme_bw()
